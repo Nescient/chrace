ae909692eae0e4668aed1342d67b128f5324ff25
nescient <antimetabole@gmail.com>
2022-04-09 21:51:54 -0400

    chore: switched to enginx and added django install

--- provision.sh
+++ provision.sh
@@ -1,2 +1,10 @@
-sudo apt install libapache2-mod-wsgi-py3
+#!/bin/sh
+
+sudo apt install vim
+#sudo apt install libapache2-mod-wsgi-py3
 sudo apt install python3 python3-venv python3-pip
+sudo apt install nginx
+
+python3 -m venv .venv
+python3 -m pip install Django
+python3 -m pip install uwsgi
