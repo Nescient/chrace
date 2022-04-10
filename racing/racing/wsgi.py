c1ab743422f9a2702a923e5dd36e45266bd401a1
nescient <antimetabole@gmail.com>
2022-04-10 18:55:58 -0400

    feat: added the racing front-end cite (removing the old stuff)

--- racing/racing/wsgi.py
+++ racing/racing/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chrace project.
+WSGI config for racing project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chrace.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'racing.settings')
 
 application = get_wsgi_application()
