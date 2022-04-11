10b086f135721a3bef54b3ea32abcbd7494279ff
nescient <antimetabole@gmail.com>
2022-04-10 20:05:50 -0400

    chore: updated to handle making the uwsgi emperor

--- provision.sh
+++ provision.sh
@@ -9,6 +9,16 @@ python3 -m venv .venv
 python3 -m pip install Django
 python3 -m pip install uwsgi
 
-
 #export PATH=$PATH:$HOME/.local/bin
+
+# link nginx to our racing front-end
 sudo ln -s /home/pi/chrace/racing/racing_nginx.conf /etc/nginx/sites-enabled/racing_nginx.conf
+
+# link uwsgi
+# create a directory for the vassals
+sudo mkdir -p /etc/uwsgi/vassals
+# symlink from the default config directory to your config file
+sudo ln -s /home/pi/chrace/racing/racing_uwsgi.ini /etc/uwsgi/vassals/
+# run the emperor
+uwsgi --emperor /etc/uwsgi/vassals --uid www-data --gid www-data
+
