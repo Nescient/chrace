de285ceaa6a045ed8ee50cace56a620b545cc054
nescient <antimetabole@gmail.com>
2022-04-10 21:41:35 -0400

    chore: moving away from env

--- provision.sh
+++ provision.sh
@@ -5,8 +5,8 @@ sudo apt install vim
 sudo apt install python3 python3-venv python3-pip
 sudo apt install nginx
 
-python3 -m venv .venv
-python3 -m pip install Django
+#python3 -m venv .venv
+sudo python3 -m pip install Django
 sudo python3 -m pip install uwsgi
 
 #export PATH=$PATH:$HOME/.local/bin
