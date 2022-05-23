52692460babb990215f2134eaca77c9f3aab7abd
nescient <antimetabole@gmail.com>
2022-05-22 22:23:55 -0400

    feat(#2): cleaned up some of the code to make it simplier

--- racers/urls.py
+++ racers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'drivers'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:driver_id>/races/', views.races, name='races'),
--- racers/views.py
+++ racers/views.py
@@ -1,27 +1,15 @@
-# from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from .models import Driver,RaceCar
 
 def index(request):
-   latest_tourney = RaceCar.objects.all()
-   context = {'latest_tourney': latest_tourney}
+   all_cars = RaceCar.objects.all()
+   context = {'all_cars': all_cars}
    return render(request, 'racers/index.html', context)
 
 def races(request, driver_id):
-   try:
-      driver = Driver.objects.get(pk=driver_id)
-   except Driver.DoesNotExist:
-      raise Http404(f'Driver {driver_id} does not exist')
+   driver = get_object_or_404(Driver, pk=driver_id)
    return render(request, 'racers/races.html', {'driver': driver})
 
 def times(request, car_id):
-   try:
-      car = RaceCar.objects.get(pk=car_id)
-      #driver = Driver.objects.get(pk=car.driver)
-   except RaceCar.DoesNotExist:
-      raise Http404(f'Car {car_id} does not exist')
-   return render(request, 'racers/times.html', {'car': car, 'driver': 'sam'})
+   car = get_object_or_404(RaceCar, pk=car_id)
+   return render(request, 'racers/times.html', {'car': car})
