e25f31b7bc180f8700624c856cf10aae7da04b67
nescient <antimetabole@gmail.com>
2022-05-22 20:11:57 -0400

    feat(#2): added string prints for our objects

--- racers/models.py
+++ racers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # this represents the human "driving" the car
 class Driver(models.Model):
@@ -6,7 +7,24 @@ class Driver(models.Model):
    last_name = models.CharField(max_length=32)
    birthday = models.DateField()
 
+   # the driver's age (as of today)
+   def age(self):
+      today = timezone.now()
+      dob = self.birthday
+      years = today.year - dob.year
+      if today.month < dob.month or (today.month == dob.month and today.day < dob.day):
+        years -= 1
+      return years
+
+
+   # a better way to represent drivers
+   def __str__(self):
+      return f'{self.first_name.capitalize()} {self.last_name.capitalize()} {self.age()}yo'
+
 # this represents a single created car
 class RaceCar(models.Model):
    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
    name = models.CharField(max_length=64)
+
+   def __str__(self):
+      return self.name
