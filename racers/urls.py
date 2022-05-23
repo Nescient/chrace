4c4299712933da8bfd42f009114b4cdf33bbf842
nescient <antimetabole@gmail.com>
2022-05-22 21:14:16 -0400

    feat(#2): added views for race car and driver

--- racers/urls.py
+++ racers/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:driver_id>/races/', views.races, name='races'),
+    path('<int:car_id>/times/', views.times, name='times'),
 ]
--- racers/views.py
+++ racers/views.py
@@ -3,7 +3,15 @@
 # Create your views here.
 
 from django.http import HttpResponse
-
+from .models import RaceCar
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the racers index.")
+   latest_tourney = RaceCar.objects.all()
+   output = ', '.join([c.name for c in latest_tourney])
+   return HttpResponse(output)
+
+def races(request, driver_id):
+    return HttpResponse(f'This is the list of all races for Driver {driver_id}.')
+
+def times(request, car_id):
+    return HttpResponse(f'This is the list of all races for Car {car_id}.')
