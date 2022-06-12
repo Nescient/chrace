b40e1a80274fd7d48570b8931a5d3aa73b13af18
nescient <antimetabole@gmail.com>
2022-06-12 17:07:34 -0400

    feat(#3): added time trial to admin

--- tourneys/admin.py
+++ tourneys/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Race, Tournament
+from .models import Race, TimeTrial, Tournament
 
 admin.site.register(Race)
+admin.site.register(TimeTrial)
 admin.site.register(Tournament)
