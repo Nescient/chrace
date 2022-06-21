b69a007590a3d03d7718c7b50ef1bc7f6ae0248a
nescient <antimetabole@gmail.com>
2022-06-21 00:04:08 -0400

    feat(#3): we are now automatically creating races with cars in lanesgit add tourneys/views.pygit add tourneys/views.py

--- tourneys/views.py
+++ tourneys/views.py
@@ -8,6 +8,7 @@ import logging
 from django.views.decorators.http import require_http_methods
 from django.apps import apps
 from django.utils import timezone
+from collections import deque
 
 class IndexView(generic.ListView):
    template_name = 'tourneys/index.html'
@@ -45,12 +46,32 @@ def edit(request, pk):
 @require_http_methods(["GET", "POST"])
 def run(request, pk):
    tourney = get_object_or_404(Tournament, pk=pk)
-   my_cars = Registration.objects.filter(tourney_id=tourney.id)
-   my_races = Race.objects.filter(tourney_id=tourney.id)
+   my_cars = tourney.registered_cars.all()
+   my_races = Race.objects.filter(tourney_id=tourney.id).order_by('id')
    
    if request.method == 'POST' and not my_races:
       # create new races based on registered cars
-      print('go')
+      print('creating new races')
+      lanes = [ deque(), deque(), deque(), deque() ]
+      for c in my_cars:
+         for a in lanes:
+            a.append(c)
+      for i,a in enumerate(lanes):
+         a.rotate(i)
+      while len(lanes[0]) > 0 or len(lanes[1]) > 0 or len(lanes[2]) > 0 or len(lanes[3]) > 0:
+         race = Race.objects.create(tourney=tourney)
+         for i,a in enumerate(lanes):
+            #race.racers.add(a[0], lane=i+1)
+            #SauceQuantity.objects.create(sandwich=chicken_teriyaki_sandwich, sauce=bbq_sauce, extra_sauce=True)
+            if len(a) > 0:
+               #race.racers.add(TimeTrial.objects.create(race=race,car=a[0],lane=i+1))
+               #tt = race.racers.add(a[0], lane=i+1)
+               #tt.lane = i + 1
+               TimeTrial.objects.create(race=race,car=a[0],lane=i+1)
+               a.popleft()
+         race = Race.objects.get(id=race.id)
+         print(race.racers.all())
+      my_races = Race.objects.filter(tourney_id=tourney.id).order_by('id')
 
    my_cars_ids = []
    for c in my_cars:
@@ -62,6 +83,13 @@ def run(request, pk):
    if request.method == 'POST':
       # get ready to rumble
       print('go')
+      for r in my_races:
+         print(r)
+         print(r.racers)
+         if not r.start_time:
+            import os
+            os.system(f'python3 /home/pi/chrace/gpio.py {r.id} &')
+            break
 
    context = {
       'tourney' : tourney,
