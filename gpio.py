f0255267c86eefa30a3cde26d3c5303e37340ed3
nescient <antimetabole@gmail.com>
2022-06-20 21:54:04 -0400

    feat(#5): added debugging stuff and django imports

--- gpio.py
+++ gpio.py
@@ -34,7 +34,26 @@ if instance_already_running():
 from gpiozero import Button
 from signal import pause
 from threading import Barrier, Event
-import time
+import time, sys
+
+CFG_EN_MID = False
+CFG_DEBUG = True
+CFG_RACE = None
+
+if CFG_DEBUG:
+   from gpiozero import Device
+   from gpiozero.pins.mock import MockFactory
+   from random import randint
+   from time import sleep
+   Device.pin_factory = MockFactory()
+
+if len(sys.argv) > 1:
+   print(f'Starting Race {sys.argv[1]}')
+   import chrace.asgi
+   from django.apps import apps
+   Race = apps.get_model('tourneys', 'Race')
+   myrace = Race.objects.get(id=sys.argv[1])
+   print(myrace)
 
 # P1:
 #    3V3  (1) (2)  5V
@@ -54,10 +73,11 @@ import time
 
 # the default pin assignments for our track
 ALL_START = Button(2)
-#LANE1_MID = Button(7)
-#LANE2_MID = Button(8)
-#LANE3_MID = Button(9)
-#LANE4_MID = Button(10)
+if CFG_EN_MID:
+   LANE1_MID = Button(7)
+   LANE2_MID = Button(8)
+   LANE3_MID = Button(9)
+   LANE4_MID = Button(10)
 LANE1_END = Button(3)
 LANE2_END = Button(4)
 LANE3_END = Button(17)
@@ -65,30 +85,37 @@ LANE4_END = Button(27)
 
 # b = Barrier(4, timeout=60.0)
 done = [ Event(), Event(), Event(), Event() ]
-a = 0
+times = []
+
 def lane1_end():
    et = time.time_ns()
+   LANE1_END.when_pressed = None
    print(f'lane1 {et}')
+   times[0] = et
    done[0].set()
    return;
 
 def lane2_end():
    et = time.time_ns()
-   a = et-start_time
+   LANE2_END.when_pressed = None
    print(f'lane2 {et}')
-   print(f'{a/(10 ** 9)}')
+   times[1] = et
    done[1].set()
    return;
 
 def lane3_end():
    et = time.time_ns()
+   LANE3_END.when_pressed = None
    print(f'lane3 {et}')
+   times[2] = et
    done[2].set()
    return;
 
 def lane4_end():
    et = time.time_ns()
+   LANE4_END.when_pressed = None
    print(f'lane4 {et}')
+   times[3] = et
    done[3].set()
    return;
 
@@ -99,20 +126,46 @@ LANE4_END.when_pressed = lane4_end
 
 # blocks until the start button goes
 print('Waiting for start')
-ALL_START.wait_for_press()
+if CFG_DEBUG:
+   sleep(1)
+else:
+   ALL_START.wait_for_press()
 start_time = time.time_ns()
-print(f'GO {start_time}')
+print(f'GO GO {start_time}')
+times = [start_time, start_time, start_time, start_time]
+
+if CFG_DEBUG:
+   sleep(randint(1,5))
+   LANE1_END.pin.drive_low()
+   LANE1_END.pin.drive_high()
+   LANE1_END.pin.drive_low()
+   LANE1_END.pin.drive_high()
+   sleep(randint(1,5))
+   LANE2_END.pin.drive_low()
+   LANE2_END.pin.drive_high()
+   LANE2_END.pin.drive_low()
+   LANE2_END.pin.drive_high()
+   sleep(randint(1,5))
+   LANE3_END.pin.drive_low()
+   LANE3_END.pin.drive_high()
+   LANE3_END.pin.drive_low()
+   LANE3_END.pin.drive_high()
+   sleep(randint(1,5))
+   LANE4_END.pin.drive_low()
+   LANE4_END.pin.drive_high()
+   LANE4_END.pin.drive_low()
+   LANE4_END.pin.drive_high()
 
 hard_stop_time = (start_time / (10 ** 9)) + 15.0
 for e in done:
-   now = time.time_ns() / (10 ** 9)
-   if now < hard_stop_time:
-      if not e.wait(hard_stop_time - now):
-         print('Timed out waiting for lane to finish!')
-   elif not e.is_set():
-      print('Timed out waiting for finish!')
+  now = time.time_ns() / (10 ** 9)
+  if now < hard_stop_time:
+     if not e.wait(hard_stop_time - now):
+        print('Timed out waiting for lane to finish!')
+  elif not e.is_set():
+     print('Timed out waiting for finish!')
 
 print('Done')
 
-b = a - start_time
-print(f'{b / (10 ** 9)}')
+for i, t in enumerate(times):
+   print(f'lane{i + 1} {(t - start_time)/(10 ** 9)}')
